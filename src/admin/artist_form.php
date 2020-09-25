d6dddf7218c804c573cdec72119fef52fa740345
starseat <starseat3@gmail.com>
2020-09-25 11:37:34 +0900

    [admin] 이미지 업로드 임시태그에 name 속성 제거

--- src/admin/artist_form.php
+++ src/admin/artist_form.php
@@ -26,28 +26,28 @@
         <form id="editArtistForm" name="editArtistForm" method="post" action="./action/artist_submit.php" enctype="multipart/form-data">
             <div class="form-row">
                 <div class="form-group col-md-4">
-                    <label for="artist_name"><b>작가명</b></label>
+                    <label for="artist_name"><strong>작가명</strong></label>
                     <input type="text" class="form-control" id="artist_name" name="artist_name">
                 </div>
                 <div class="form-group col-md-4">
-                    <label for="artist_name_en"><b>작가명 (영문)</b></label>
+                    <label for="artist_name_en"><strong>작가명 (영문)</strong></label>
                     <input type="text" class="form-control" id="artist_name_en" name="artist_name_en">
                 </div>
                 <div class="form-group col-md-4">
-                    <label for="artist_year"><b>년도</b></label>
+                    <label for="artist_year"><strong>년도</strong></label>
                     <input type="number" class="form-control" id="artist_year" name="artist_year">
                 </div>
             </div>
             <hr>
             <div class="form-group">
-                <label for="artist_thumbnail" id="artist_thumbnail_label"><b>썸네일</b></label>
+                <label for="artist_thumbnail" id="artist_thumbnail_label"><strong>썸네일</strong></label>
                 <div id="artist_thumbnail"></div>
             </div>
             <hr>
             <div class="form-row">
                 <div class="form-group col-md-5">
-                    <label for="artist_image1"><b>대표 이미지 1</b></label>
-                    <div id="artist_image1" name="artist_image1"></div>
+                    <label for="artist_image1"><strong>대표 이미지 1</strong></label>
+                    <div id="artist_image1"></div>
                     <div class="row artist_image_caption_box">
                         <div class="col-2"><label for="artist_image1_caption" id="artist_image1_caption" class="artist_image_caption_label">캡션</label></div>
                         <div class="col-10"><input type="text" id="artist_image1_caption" class="form-control artist_image_caption"></div>
@@ -55,8 +55,8 @@
                 </div>
                 <div class="form-group col-md-1"></div>
                 <div class="form-group col-md-5">
-                    <label for="artist_image2"><b>대표 이미지 2</b></label>
-                    <div id="artist_image2" name="artist_image2"></div>
+                    <label for="artist_image2"><strong>대표 이미지 2</strong></label>
+                    <div id="artist_image2"></div>
                     <div class="row artist_image_caption_box">
                         <div class="col-2"><label for="artist_image2_caption" id="artist_image2_caption" class="artist_image_caption_label">캡션</label></div>
                         <div class="col-10"><input type="text" id="artist_image2_caption" class="form-control artist_image_caption"></div>
@@ -65,8 +65,8 @@
             </div>
             <div class="form-row">
                 <div class="form-group col-md-5">
-                    <label for="artist_image3"><b>대표 이미지 3</b></label>
-                    <div id="artist_image3" name="artist_image3"></div>
+                    <label for="artist_image3"><strong>대표 이미지 3</strong></label>
+                    <div id="artist_image3"></div>
                     <div class="row artist_image_caption_box">
                         <div class="col-2"><label for="artist_image3_caption" id="artist_image3_caption" class="artist_image_caption_label">캡션</label></div>
                         <div class="col-10"><input type="text" id="artist_image3_caption" class="form-control artist_image_caption"></div>
@@ -74,8 +74,8 @@
                 </div>
                 <div class="form-group col-md-1"></div>
                 <div class="form-group col-md-5">
-                    <label for="artist_image4"><b>대표 이미지 4</b></label>
-                    <div id="artist_image4" name="artist_image4"></div>
+                    <label for="artist_image4"><strong>대표 이미지 4</strong></label>
+                    <div id="artist_image4"></div>
                     <div class="row artist_image_caption_box">
                         <div class="col-2"><label for="artist_image4_caption" id="artist_image4_caption" class="artist_image_caption_label">캡션</label></div>
                         <div class="col-10"><input type="text" id="artist_image4_caption" class="form-control artist_image_caption"></div>
@@ -84,27 +84,27 @@
             </div>
             <hr>
             <div class="form-group">
-                <div class="form-row form-input-title-box"><label for="artist_introduction"><b>작가 소개</b></label></div>
+                <div class="form-row form-input-title-box"><label for="artist_introduction"><strong>작가 소개</strong></label></div>
                 <div id="artist_introduction" class="form-control"></div>
             </div>
             <hr>
             <div class="form-group">
-                <div class="form-row form-input-title-box"><label for="artist_academic"><b>학력</b></label></div>
+                <div class="form-row form-input-title-box"><label for="artist_academic"><strong>학력</strong></label></div>
                 <div id="artist_academic" class="form-control"></div>
             </div>
             <hr>
             <div class="form-group">
-                <div class="form-row form-input-title-box"><label for="artist_individual_exhibition"><b>주요 개인전</b></label></div>
+                <div class="form-row form-input-title-box"><label for="artist_individual_exhibition"><strong>주요 개인전</strong></label></div>
                 <div id="artist_individual_exhibition" class="form-control"></div>
             </div>
             <hr>
             <div class="form-group">
-                <div class="form-row form-input-title-box"><label for="artist_team_competition"><b>주요 단체전</b></label></div>
+                <div class="form-row form-input-title-box"><label for="artist_team_competition"><strong>주요 단체전</strong></label></div>
                 <div id="artist_team_competition" class="form-control"></div>
             </div>
             <hr>
             <div class="form-group">
-                <div class="form-row form-input-title-box"><label for="artist_interview"><b>작가 인터뷰</b></label></div>
+                <div class="form-row form-input-title-box"><label for="artist_interview"><strong>작가 인터뷰</strong></label></div>
                 <div id="artist_interview" class="form-control"></div>
             </div>
         </form>
