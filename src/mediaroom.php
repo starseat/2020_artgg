dae18ccee9328297ce8f45cc247fb2d577efd8d7
jw.lee <starseat3@gmail.com>
2020-10-09 19:54:35 +0900

    [dev] 미디어룸, 뷰잉룸 추가

---
+++ src/mediaroom.php
@@ -0,0 +1,21 @@
+<?php require_once('./fragment/header.php'); ?>
+
+<!-- 콘텐츠 -->
+<div class="container">
+    <div class="content_inner">
+        <div class="section_box_w">
+            <h2 class="content_title">Program (sample)</h2>
+            <h3 class="content_title_d">미디어룸</h3>
+            <!-- 미디어룸 -->
+            <div class="section_viewing_w">
+                <div class="viewing_cont_w">
+                    <iframe width="100%" height="600" src="https://my.matterport.com/show/?m=AgtaYshBYQV&amp;help=1" frameborder="0" allowfullscreen=""></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require_once('./fragment/footer.php'); ?>
+
+<?php require_once('./fragment/tail.php'); ?>
---
+++ src/viewingroom.php
@@ -0,0 +1,34 @@
+<?php require_once('./fragment/header.php'); ?>
+
+<!-- 콘텐츠 -->
+<div class="container">
+    <div class="content_inner">
+        <div class="section_box_w">
+            <h2 class="content_title">Program (sample)</h2>
+            <h3 class="content_title_d">이광기의 온라인 아트쇼</h3>
+            <!-- 뷰잉룸 -->
+            <div class="section_viewing_w">
+                <div class="viewing_info_w">
+                    <div class="viewing_image_w">
+                        <img src="//via.placeholder.com/170x96" alt="" class="viewing_img">
+                    </div>
+                    <div class="viewing_text_w">
+                        <strong class="vt_title">
+                            타이틀 타이틀
+                            <span class="vt_sub_title">Gu Nayoung (b.1983)</span>
+                        </strong>
+                        <span class="vt_name"> 전시기간 : 2019/10/08 ~ 2019/10/15</span>
+                        <span class="vt_text">아트경기2019 X Union Art Fair 2019</span>
+                    </div>
+                </div>
+                <div class="viewing_cont_w">
+                    <iframe width="100%" height="600" src="https://my.matterport.com/show/?m=AgtaYshBYQV&amp;help=1" frameborder="0" allowfullscreen=""></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require_once('./fragment/footer.php'); ?>
+
+<?php require_once('./fragment/tail.php'); ?>
