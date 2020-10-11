980c1b300273563271ba8dc87b960160d3855927
jw.lee <starseat3@gmail.com>
2020-10-11 17:00:52 +0900

    [admin] index 페이지에서 main.php 로 리다이렉트

--- src/admin/index.php
+++ src/admin/index.php
@@ -1,5 +1,9 @@
 <?php require_once('fragment/content_layout.php'); ?>
 
+<script>
+    location.href = './main.php';
+</script>
+
 <h1 class="mt-4">Dashboard</h1>
 <ol class="breadcrumb mb-4">
     <li class="breadcrumb-item active">Dashboard</li>
