861228f02a582b0384631586f8484aea5d919046
jw.lee <starseat3@gmail.com>
2020-10-12 23:33:57 +0900

    [dev] 보도자료 임시 페이지 추가

---
+++ src/press_release.php
@@ -0,0 +1,19 @@
+<?php require_once('./fragment/header.php'); ?>
+
+<!-- 콘텐츠 -->
+<div class="container">
+    <div class="content_inner">
+        <div class="section_box_w">
+            <h2 class="content_title">About</h2>
+            <h3 class="content_title_d">보도 자료</h3>
+            <div class="section_viewing_w">
+                <div class="viewing_info_w">
+                준비중 입니다.
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require_once('./fragment/footer.php'); ?>
+
+<?php require_once('./fragment/tail.php'); ?>
