23e5e47caa93f4a99c05c4016968a13c8d01ff28
jw.lee <starseat3@gmail.com>
2020-10-14 00:11:59 +0900

    [dev] 보도자료 페이지 수정

--- src/press_release.php
+++ src/press_release.php
@@ -4,14 +4,35 @@
 <div class="container">
     <div class="content_inner">
         <div class="section_box_w">
-            <h2 class="content_title">About</h2>
-            <h3 class="content_title_d">보도 자료</h3>
-            <div class="section_viewing_w">
-                <div class="viewing_info_w">
-                준비중 입니다.
+            <h2 class="content_title">보도자료</h2>
+            <!-- <h3 class="content_title_d">서브텍스트있을경우해당태그사용</h3> -->
+            <!-- 보도자료 -->
+            <div class="section_report_w">
+                <ul class="text_link_list">
+                    <?php
+
+                    $sql = "SELECT title, link FROM artgg_press_release ORDER BY seq DESC";
+                    $result = mysqli_query($conn, $sql) or exit(mysqli_error($conn));
+
+                    while ($row = $result->fetch_array()) {
+                        echo ('<li class="tl_inner">');
+                        echo ('    <a href="' . RemoveXSS($row['link']) . '" class="tl_cont" target="_blank" >');
+                        echo ('        <span class="text">' . RemoveXSS($row['title']) . '</span>');
+                        echo ('        <span class="line -right"></span>');
+                        echo ('        <span class="line -top"></span>');
+                        echo ('        <span class="line -left"></span>');
+                        echo ('        <span class="line -bottom"></span>');
+                        echo ('    </a>');
+                        echo ('</li>');
+                    }
+
+                    $result->free();
+                    ?>
+                </ul>
             </div>
         </div>
     </div>
+
 </div>
 
 <?php require_once('./fragment/footer.php'); ?>
