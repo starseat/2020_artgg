e7782930d402cbb343dcdad6d6bfe52bfa8cb22e
starseat <starseat3@gmail.com>
2020-10-14 13:49:31 +0900

    [dev] Open graph 정보 수정

--- src/fragment/head.php
+++ src/fragment/head.php
@@ -5,12 +5,13 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="description" content="desc">
     <title>2020 아트경기</title>
+    <meta name="description" content="아트경기 Art Gyeonggi ">
     <meta property="og:type" content="website">
-    <meta property="og:title" content="title">
-    <meta property="og:description" content="deesc">
-    <meta property="og:url" content="url">
+    <meta property="og:title" content="아트경기 Art Gyeonggi ">
+    <meta property="og:description" content="아트경기 Art Gyeonggi ">
+    <meta property="og:url" content="http://artgg.ggcf.kr/">
+    <meta property="og:image" content="./img/business_artgg.jpg" />
 
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/moonspam/NanumSquare@1.0/nanumsquare.css">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
