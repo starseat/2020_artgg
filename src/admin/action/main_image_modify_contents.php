00d180551a11c21b5cc7966e059fdbcedd9eee05
jw.lee <starseat3@gmail.com>
2020-10-14 22:33:54 +0900

    [admin] 메인 이미지 링크 추가 처리가 원복되서 다시 등록

--- src/admin/action/main_image_modify_contents.php
+++ src/admin/action/main_image_modify_contents.php
@@ -17,6 +17,8 @@ if(isEmpty($content_list)) {
     $result_array['message'] = '잘못된 컨텐츠 정보 입니다.';
     mysqli_close($conn);
     echo json_encode($result_array, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    mysqli_close($conn);
+    flush();
     exit;
 }
 
@@ -26,11 +28,16 @@ if($content_count == 0) {
     $result_array['message'] = '변경될 컨텐츠 정보가 없습니다.';
     mysqli_close($conn);
     echo json_encode($result_array, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    mysqli_close($conn);
+    flush();
     exit;
 }
 
 for($i=0; $i<$content_count; $i++) {
-    $sql  = "UPDATE artgg_image SET sort = " . $content_list[$i]['sort'] . " WHERE seq = " . $content_list[$i]['seq'];
+    $sql  = "UPDATE artgg_image SET ";
+    $sql .= "sort = " . $content_list[$i]['sort'] . ", ";
+    $sql .= "link = '" . $content_list[$i]['link'] . "' ";
+    $sql .= "WHERE seq = " . $content_list[$i]['seq'];
     $result = mysqli_query($conn, $sql) or exit(mysqli_error($conn));
 }
 
