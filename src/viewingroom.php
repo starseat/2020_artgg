af5806e06616fe25ab25de0a363ea032b68e7fd6
starseat <starseat3@gmail.com>
2020-10-16 10:52:04 +0900

    [dev] 뷰잉룸 임시 위치 수정

--- src/viewingroom.php
+++ src/viewingroom.php
@@ -1,5 +1,9 @@
 <?php require_once('./fragment/header.php'); ?>
 
+<style>
+.title-box { margin: 4rem; }
+.viewing_cont_w { text-align: center; }
+</style>
 <!-- 콘텐츠 -->
 <div class="container">
     <div class="content_inner">
@@ -8,19 +12,20 @@
             <h3 class="content_title_d">뷰잉룸</h3>
             <!-- 뷰잉룸 -->
             <div class="section_viewing_w">
-                <div class="viewing_info_w">
+                <!-- <div class="viewing_info_w">
                     <div class="viewing_image_w">
                         <img src="//via.placeholder.com/170x96" alt="" class="viewing_img">
                     </div>
                     <div class="viewing_text_w">
                         <strong class="vt_title">
                             아트경기 2020
-                            <!-- <span class="vt_sub_title">Gu Nayoung (b.1983)</span> -->
+                            <span class="vt_sub_title">Gu Nayoung (b.1983)</span>
                         </strong>
-                        <!-- <span class="vt_name"> 전시기간 : 2019/10/08 ~ 2019/10/15</span>
-                        <span class="vt_text">아트경기2019 X Union Art Fair 2019</span> -->
+                        <span class="vt_name"> 전시기간 : 2019/10/08 ~ 2019/10/15</span>
+                        <span class="vt_text">아트경기2019 X Union Art Fair 2019</span>
                     </div>
-                </div>
+                </div> -->
+                <div class="title-box"><strong class="vt_title">아트경기 2020</strong></div>
                 <div class="viewing_cont_w">
                     <iframe width='853' height='480' src='https://my.matterport.com/show/?m=oVbQo1a8HsC' frameborder='0' allowfullscreen allow='xr-spatial-tracking'></iframe>
                 </div>
