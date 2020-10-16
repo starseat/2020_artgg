07d57be235571318e572839a791925f6ce84baa4
starseat <starseat3@gmail.com>
2020-10-16 10:46:20 +0900

    [dev] 팝업 공지사항이동 추가 (임시 팝업임), 뷰잉룸 정보 추가

--- src/viewingroom.php
+++ src/viewingroom.php
@@ -1,40 +1,36 @@
 <?php require_once('./fragment/header.php'); ?>
 
 <!-- 콘텐츠 -->
-<?php
-// 이전 화면 저장용
-// <div class="container">
-//     <div class="content_inner">
-//         <div class="section_box_w">
-//             <h2 class="content_title">Space</h2>
-//             <h3 class="content_title_d">뷰잉룸</h3>
-//             <!-- 뷰잉룸 -->
-//             <div class="section_viewing_w">
-//                 <div class="viewing_info_w">
-//                     <!-- <div class="viewing_image_w">
-//                         <img src="//via.placeholder.com/170x96" alt="" class="viewing_img">
-//                     </div>
-//                     <div class="viewing_text_w">
-//                         <strong class="vt_title">
-//                             타이틀 타이틀
-//                             <span class="vt_sub_title">Gu Nayoung (b.1983)</span>
-//                         </strong>
-//                         <span class="vt_name"> 전시기간 : 2019/10/08 ~ 2019/10/15</span>
-//                         <span class="vt_text">아트경기2019 X Union Art Fair 2019</span>
-//                     </div>
-//                 </div>
-//                 <div class="viewing_cont_w">
-//                     <iframe width="100%" height="600" src="https://my.matterport.com/show/?m=AgtaYshBYQV&amp;help=1" frameborder="0" allowfullscreen=""></iframe>
-//                 </div> -->
-//                 준비중 입니다.
-//             </div>
-//         </div>
-//     </div>
-// </div>
-?>
+<div class="container">
+    <div class="content_inner">
+        <div class="section_box_w">
+            <h2 class="content_title">Space</h2>
+            <h3 class="content_title_d">뷰잉룸</h3>
+            <!-- 뷰잉룸 -->
+            <div class="section_viewing_w">
+                <div class="viewing_info_w">
+                    <div class="viewing_image_w">
+                        <img src="//via.placeholder.com/170x96" alt="" class="viewing_img">
+                    </div>
+                    <div class="viewing_text_w">
+                        <strong class="vt_title">
+                            아트경기 2020
+                            <!-- <span class="vt_sub_title">Gu Nayoung (b.1983)</span> -->
+                        </strong>
+                        <!-- <span class="vt_name"> 전시기간 : 2019/10/08 ~ 2019/10/15</span>
+                        <span class="vt_text">아트경기2019 X Union Art Fair 2019</span> -->
+                    </div>
+                </div>
+                <div class="viewing_cont_w">
+                    <iframe width='853' height='480' src='https://my.matterport.com/show/?m=oVbQo1a8HsC' frameborder='0' allowfullscreen allow='xr-spatial-tracking'></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <!-- 콘텐츠 -->
-<div class="container">
+<!-- <div class="container">
     <div class="content_inner">
         <div class="section_box_w">
             <h2 class="content_title">뷰잉룸</h2>
@@ -43,7 +39,7 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 <?php require_once('./fragment/footer.php'); ?>
 
 <?php require_once('./fragment/tail.php'); ?>
