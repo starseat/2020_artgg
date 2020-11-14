41c64fd4f9f0c113959a5f250c925d5e761f82e6
jw.lee <starseat3@gmail.com>
2020-11-14 16:55:25 +0900

    [admin] 보도자료 페이징 처리 추가

--- src/admin/press_release.php
+++ src/admin/press_release.php
@@ -5,6 +5,21 @@
 include('common.php');
 include('db_conn.php');
 
+// 게시굴 수
+$item_row_count = 10;
+// 하단 페이지 block 수 (1, 2, 3, 4, ...  이런거)
+$page_block_count = 10;
+
+$sql = "SELECT COUNT(*) FROM artgg_press_release WHERE deleted_at IS NULL";
+
+$result = mysqli_query($conn, $sql) or exit(mysqli_error($conn));
+$total_count = mysqli_fetch_array($result);
+$total_count = intval($total_count[0]);
+$result->free();
+
+// 현재 페이지
+$page = isset($_GET['page']) ? trim($_GET['page']) : 1;
+$paging_info = getPagingInfo($page, $total_count, $item_row_count, $page_block_count);
 
 ?>
 
@@ -60,7 +75,7 @@ include('db_conn.php');
         <tbody>
             <?php
 
-            $sql  = "SELECT seq, title, link FROM artgg_press_release WHERE deleted_at IS NULL ORDER BY seq DESC";
+            $sql  = "SELECT seq, title, link FROM artgg_press_release WHERE deleted_at IS NULL ORDER BY seq desc LIMIT " . $paging_info['page_db'] . ", $item_row_count";
             $result = mysqli_query($conn, $sql) or exit(mysqli_error($conn));
             $press_release_length = $result->num_rows;
 
@@ -83,6 +98,40 @@ include('db_conn.php');
             ?>
         </tbody>
     </table>
+
+    <div class="mt-4 d-flex justify-content-center">
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <?php
+                if ($paging_info['page_prev'] > 0) {
+                    echo ('<li class="page-item">');
+                    echo ('    <a class="page-link page-link-dark" href="./press_release.php?page=' . $paging_info['page_prev'] . '" aria-label="Previous">');
+                    echo ('        <span aria-hidden="true">&laquo;</span>');
+                    echo ('    </a>');
+                    echo ('</li>');
+                }
+                ?>
+                <?php
+                for ($i = $paging_info['page_start']; $i <= $paging_info['page_end']; $i++) {
+                    if ($i == $page) {
+                        echo ('<li class="page-item active-dark"><a class="page-link page-link-dark" href="#">' . $i . '</a></li>');
+                    } else {
+                        echo ('<li class="page-item"><a class="page-link page-link-dark" href="./press_release.php?page=' . $i . '" >' . $i . '</a></li>');
+                    }
+                }
+                ?>
+                <?php
+                if ($paging_info['page_next'] < $paging_info['page_total']) {
+                    echo ('<li class="page-item">');
+                    echo ('    <a class="page-link page-link-dark" href="./press_release.php?page=' . $paging_info['page_next'] . '" aria-label="Next">');
+                    echo ('        <span aria-hidden="true">&raquo;</span>');
+                    echo ('    </a>');
+                    echo ('</li>');
+                }
+                ?>
+            </ul>
+        </nav>
+    </div>
 
     <div class="m-4 d-flex justify-content-end">
         <div class="row"><button type="button" class="btn btn-info" onclick="showEditPressReleaseModal();">추가</button></div>
